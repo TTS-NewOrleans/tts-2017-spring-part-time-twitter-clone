e3cd570af30c42b8599ac248fd7013dad2a86dae
Ryan Hageman <improv42@gmail.com>
2017-07-05 20:18:52 -0500

    Add ability to update tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,26 @@
 class TweetsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_tweet, only: [:show, :edit, :update]
 
   def index
     @tweets = Tweet.all
   end
 
   def show
-    @tweet = Tweet.find(params[:id])
   end
 
   def edit
   end
+
+  def update
+    respond_to do |format|
+      if @tweet.update(tweet_params)
+        format.html { redirect_to @tweet, notice: 'Tweet was updated.' }
+      else
+        format.html { render :edit }
+      end # end if
+    end # end do
+  end # end update
 
   def new
     @tweet = Tweet.new
@@ -33,6 +43,10 @@ class TweetsController < ApplicationController
 
 
   private
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
 
   def tweet_params
     params.require(:tweet).permit(:message, :user_id)
